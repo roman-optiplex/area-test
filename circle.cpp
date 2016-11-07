06abfa4718be5eceaa98db64d9ee60e5c0d303f2
roman-optiplex <roman.optiplex@gmail.com>
2016-11-07 05:10:05 +0100

    Merge triangle and circle.

--- circle.cpp
+++ circle.cpp
@@ -6,8 +6,22 @@ int main( )
 { 
     float PI = 3.14;                // variables can be initialized during declaration 
     int rad; 
+    double a;
+    double b;
+    double c;
     cout<< "Enter the radius"; 
     cin>>rad; 
     cout<< "Area of the circle is: "<< PI * rad * rad<<endl; 
-    return 0;
+    
+    cout<< "Enter a"; 
+    cin>>a;
+    cout<< "Enter b"; 
+    cin>>b;
+    cout<< "Enter c"; 
+    cin>>c;
+ 
+    cout<< "Perimeter of the triangle is: "<< a+b+c;
+
+return 0;
+
 } 
